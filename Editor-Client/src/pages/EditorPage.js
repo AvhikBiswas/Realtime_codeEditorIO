03574044b90523df777dabbcdab2045ba5f7fbe6
AvhikBiswas <abhikbiswas351@gmail.com>
2024-02-24 22:59:18 +0530

    feat: Disconnect User Removed

--- Editor-Client/src/pages/EditorPage.js
+++ Editor-Client/src/pages/EditorPage.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import toast from 'react-hot-toast';
-import io from 'socket.io-client';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
-import UserAvatar from '../components/UserAvatar';
-import Editor from '../components/Editor';
-import UserName from '../components/UserName';
-import { SignInNewUser, UserLeave, GetAllUser } from '../api/User_API';
-import { useSelector } from 'react-redux';
-import './editorpage.css';
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import io from "socket.io-client";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import UserAvatar from "../components/UserAvatar";
+import Editor from "../components/Editor";
+import UserName from "../components/UserName";
+import { SignInNewUser, UserLeave, GetAllUser } from "../api/User_API";
+import { useSelector } from "react-redux";
+import "./editorpage.css";
 
 export default function EditorPage() {
   const navigate = useNavigate();
@@ -17,66 +17,83 @@ export default function EditorPage() {
   const { roomId } = useParams();
   const [allUserData, setAllUserData] = useState([]);
   const [showUserName, setShowUserName] = useState(false);
-  const [output, setOutput] = useState('');
+  const [output, setOutput] = useState("");
   const [loading, setLoading] = useState(false);
 
   const socketRef = useRef(null);
-  const codeRef = useRef('');
+  const codeRef = useRef("");
   const [ownName, setOwnName] = useState(userName);
   const editorValue = useSelector((state) => state.EditorState.value);
-  const inputRef = useRef('');
+  const inputRef = useRef("");
 
   useEffect(() => {
+    if (!ownName) {
+      toast.error("Join with your Name");
+      navigate("/");
+    }
     const init = async () => {
-      socketRef.current = io.connect('http://localhost:3030');
+      socketRef.current = io.connect("http://localhost:3030");
 
-      socketRef.current.on('connect_error', (err) => handleErrors(err));
-      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
 
       try {
+        await SignInNewUser(ownName, roomId);
         const users = await GetAllUser(roomId);
         setAllUserData(users.data);
       } catch (error) {
-        console.error('Error during user initialization:', error);
+        console.error("Error during user initialization:", error);
       }
 
       function handleErrors(e) {
-        console.log('Socket error', e);
-        toast.error('Socket connection failed, try again later.');
-        navigate('/');
+        console.log("Socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        navigate("/");
       }
 
-      socketRef.current.emit('join_room', {
+      socketRef.current.emit("join_room", {
         ownName,
         roomId,
       });
 
-      socketRef.current.on('JOINED', async ({ Ownname }) => {
+      socketRef.current.on("JOINED", async ({ Ownname }) => {
         if (Ownname !== location.state?.userName) {
           try {
             const users = await GetAllUser(roomId);
             setAllUserData(users.data);
           } catch (error) {
-            console.error('Error during user initialization:', error);
+            console.error("Error during user initialization:", error);
           }
           toast.success(`${Ownname} joined the room.`);
           console.log(`${Ownname} joined`);
         }
 
-        socketRef.current.emit('SYNC_CODE', {
+        socketRef.current.emit("SYNC_CODE", {
           code: codeRef.current,
           roomId,
         });
       });
 
-      socketRef.current.on('RUNED', ({ code }) => {
-        console.log('Received RUNED event:', code);
+      socketRef.current.on("USERS_UPDATE", async (data) => {
+        console.log("user left name ", data);
+
+        const userData = await GetAllUser(roomId);
+        await toast.success(`${data} Left The Room`);
+        setAllUserData(userData.data);
+      });
+
+      socketRef.current.on("RUNED", ({ code }) => {
+        console.log("Received RUNED event:", code);
         setLoading(false);
         setOutput(code);
       });
 
-      socketRef.current.on('DISCONNECTED', ({ Ownname }) => {
-        toast.success(`${Ownname} left the room.`);
+      socketRef.current.on("DISCONNECTED", async ({ Ownname, room }) => {
+        const data = { ownName, roomId };
+        await UserLeave(data);
+        const userData = await GetAllUser(roomId);
+        console.log(`${Ownname} left the room.`);
+        setAllUserData(userData.data);
       });
     };
 
@@ -85,23 +102,23 @@ export default function EditorPage() {
     return () => {
       if (socketRef.current) {
         socketRef.current.disconnect();
-        socketRef.current.off('JOINED');
+        socketRef.current.off("JOINED");
       }
     };
   }, []);
 
   const handleCopyToClipboard = () => {
     navigator.clipboard.writeText(roomId);
-    toast.success('Room link copied to clipboard!');
+    toast.success("Room link copied to clipboard!");
   };
 
   const handleRun = () => {
-    if(editorValue.trim()===''){
-      setOutput('ENTER THE CODE');
+    if (editorValue.trim() === "") {
+      setOutput("ENTER THE CODE");
       return;
     }
     setLoading(true);
-    socketRef.current.emit('RUN', {
+    socketRef.current.emit("RUN", {
       roomId,
       code: editorValue,
       input: inputRef.current,
@@ -109,65 +126,72 @@ export default function EditorPage() {
   };
 
   const handleLeave = async () => {
+    if (!ownName || !roomId) {
+      return;
+    }
+
+    // Emit a Left event to the socket server before leaving the room
+    socketRef.current.emit("Left", { ownName, roomId });
+
     const data = { ownName, roomId };
     await UserLeave(data);
-    navigate('/');
+    navigate("/");
   };
 
   const handleInput = (e) => {
     inputRef.current = e.target.value;
   };
 
-  const linkedInUrl = 'https://www.linkedin.com/in/avhik/';
-  const githubUrl = 'https://github.com/AvhikBiswas';
+  const linkedInUrl = "https://www.linkedin.com/in/avhik/";
+  const githubUrl = "https://github.com/AvhikBiswas";
 
   return (
-    <div className='flex flex-col h-screen bg-gray-900 mt-2'>
+    <div className="flex flex-col h-screen bg-gray-900 mt-2">
       {showUserName ? (
         <UserName />
       ) : (
         <>
-          <div className='flex justify-end mb-4'>
-          <h1 className='text-white'>Connected Users</h1>
-
+          <div className="flex justify-end mb-4">
             <button
               onClick={handleCopyToClipboard}
-              className='bg-blue-500 text-white px-4 py-2 rounded m-1'
+              className="bg-blue-500 text-white px-4 py-2 rounded m-1"
             >
               Share Room
             </button>
             <button
               onClick={handleLeave}
-              className='bg-red-800 text-white px-4 py-2 rounded m-1'
+              className="bg-red-800 text-white px-4 py-2 rounded m-1"
             >
               Leave
             </button>
-            <select className='w-32 p-2 border rounded text-white bg-gray-800 m-1'>
-              <option value='cpp'>C++</option>
+            <select className="w-32 p-2 border rounded text-white bg-gray-800 m-1">
+              <option value="cpp">C++</option>
             </select>
             <button
               onClick={handleRun}
               className={`bg-green-500 text-white px-4 py-2 rounded mr-2 m-1 ${
-                loading ? 'opacity-50 cursor-not-allowed' : ''
+                loading ? "opacity-50 cursor-not-allowed" : ""
               }`}
               disabled={loading}
             >
-              {loading ? 'Running...' : 'Run'}
+              {loading ? "Running..." : "Run"}
             </button>
           </div>
-
-          <div className='flex flex-1 p-3'>
-            <div className='flex w-1/5'>
-              <div className='users flex flex-wrap'>
-                {allUserData.length !== 0 ? (
-                  allUserData.map((item) => (
-                    <UserAvatar key={item._id} user={item} />
-                  ))
-                ) : null}
+          <div className="text-white text-lg text-center flex justify-start items-center ml-5">
+            Connected User
+          </div>
+          <div className="flex flex-1 p-3">
+            <div className="flex w-1/5 m-2">
+              <div className="users flex flex-wrap">
+                {allUserData.length !== 0
+                  ? allUserData.map((item) => (
+                      <UserAvatar key={item._id} user={item} />
+                    ))
+                  : null}
               </div>
             </div>
 
-            <div className='w-3/5 mb-5'>
+            <div className="w-3/5 mb-5">
               <Editor
                 socketRef={socketRef}
                 roomId={roomId}
@@ -177,45 +201,45 @@ export default function EditorPage() {
               />
             </div>
 
-            <div className='w-1/5 p-2 border-l border-gray-800'>
-              <div className='bg-gray-800 rounded text-white mb-4 p-2'>
+            <div className="w-1/5 p-2 border-l border-gray-800">
+              <div className="bg-gray-800 rounded text-white mb-4 p-2">
                 Input Terminal
               </div>
               <textarea
                 onChange={handleInput}
-                rows='10'
-                className='w-full p-2 border rounded mb-4 bg-gray-800 text-white'
-                placeholder='Enter code here...'
+                rows="10"
+                className="w-full p-2 border rounded mb-4 bg-gray-800 text-white"
+                placeholder="Enter code here..."
               />
-              <div className='bg-gray-800 rounded text-white p-2'>
+              <div className="bg-gray-800 rounded text-white p-2">
                 Output Terminal
               </div>
-              <div className='w-full p-2 mt-2 border rounded mb-4 bg-gray-800 text-white h-52'>
+              <div className="w-full p-2 mt-2 border rounded mb-4 bg-gray-800 text-white h-52">
                 <p>{output}</p>
               </div>
             </div>
           </div>
 
-          <div className='p-4 h-full w-full text-white flex items-center justify-between rounded border-t border-gray-800'>
-            <div className='flex items-center'>
-              <div className='mr-2 text-xs bg-gray-800 p-2 rounded-md'>
+          <div className="p-4 h-full w-full text-white flex items-center justify-between rounded border-t border-gray-800">
+            <div className="flex items-center">
+              <div className="mr-2 text-xs bg-gray-800 p-2 rounded-md">
                 <p>Note: This app is still under development.</p>
                 <p>Please let me know if you find bugs out here.</p>
-                <div className='flex items-center mt-1'>
-                  <p className=''>Connect with me:</p>
+                <div className="flex items-center mt-1">
+                  <p className="">Connect with me:</p>
                   <a
                     href={linkedInUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-blue-500 hover:underline ml-2'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline ml-2"
                   >
                     <FaLinkedin size={30} />
                   </a>
                   <a
                     href={githubUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-gray-500 hover:underline ml-2'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 hover:underline ml-2"
                   >
                     <FaGithub size={30} />
                   </a>
@@ -224,7 +248,7 @@ export default function EditorPage() {
             </div>
 
             <div>
-              <p className='text-sm'>&copy; Avhik Biswas</p>
+              <p className="text-sm">&copy; Avhik Biswas</p>
             </div>
           </div>
         </>
--- Editor_Server/src/index.js
+++ Editor_Server/src/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const connectDB = require('./config/database.js');
-const socketIO = require('socket.io');
-const bodyParser = require('body-parser');
-const Api_Routes = require('./routes/index');
-const mongoose = require('mongoose');
-const { compile } = require('./service/compile_service.js');
-
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const connectDB = require("./config/database.js");
+const socketIO = require("socket.io");
+const bodyParser = require("body-parser");
+const Api_Routes = require("./routes/index");
+const mongoose = require("mongoose");
+const { compile } = require("./service/compile_service.js");
+const RemoveUser = require("./service/LeftUser_service.js");
+const GetAllActiveUser_Service = require("./service/ActiveUser_service");
 const port = 3030;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
@@ -24,58 +25,78 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Socket.io connection handling
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  const removeUser = new RemoveUser();
+
   console.log(`User connected: ${socket.id}`);
-  socket.on('join_room', (data) => {
+  socket.on("join_room", (data) => {
     const { ownName, roomId } = data;
     console.log(`---------own name -----> ${ownName}`);
     socket.join(roomId);
+    socket.ownName = ownName;
+    socket.roomId = roomId;
     console.log(`User ${ownName} joined room ${roomId}`);
-    socket.to(roomId).emit('JOINED', { Ownname: ownName, room: roomId });
+    socket.to(roomId).emit("JOINED", { Ownname: ownName, room: roomId });
   });
 
-  socket.on('SYNC_CODE', (data) => {
+  socket.on("SYNC_CODE", (data) => {
     const { code, roomId } = data;
     console.log(`from sync_code roomid and code ${roomId} in room ${code}`);
-    io.to(roomId).emit('CODE_CHANGE', { code });
+    io.to(roomId).emit("CODE_CHANGE", { code });
   });
 
-  socket.on('CODE_CHANGE', (data) => {
+  socket.on("CODE_CHANGE", (data) => {
     const { code, roomId } = data;
     console.log(`from code_change roomid and code ${roomId} in room ${code}`);
-    socket.in(roomId).emit('CODE_CHANGE', { code });
+    socket.in(roomId).emit("CODE_CHANGE", { code });
   });
 
-  socket.on('RUN', async (data) => {
-    const { code, roomId,input } = data;
+  socket.on("RUN", async (data) => {
+    const { code, roomId, input } = data;
     console.log(`Received RUN event for roomId ${code}`);
-    const res = await compile(code,input);
-    console.log('Response from compile:', res);
-    io.to(roomId).emit('RUNED', { code: res });
+    const res = await compile(code, input);
+    console.log("Response from compile:", res);
+    io.to(roomId).emit("RUNED", { code: res });
   });
-  
 
-  socket.on('DISCONNECTING', (data) => {
+  socket.on("Left", async (data) => {
     const { ownName, roomId } = data;
-    socket.to(roomId).emit('DISCONNECTED', { Ownname: ownName, room: roomId });
+    console.log(`User ${ownName} left room ${roomId}`);
+
+    // Remove the user from the database
+    await removeUser.LeftUser(data);
+
+    // Update the list of users in the room
+    io.to(roomId).emit("USERS_UPDATE", ownName);
   });
 
+  socket.on("disconnect", async () => {
+    console.log(`User disconnected: ${socket.id}`);
+    socket.disconnected = true;
+    const data = {
+      ownName: socket.ownName,
+      roomId: socket.roomId,
+    };
+    await removeUser.LeftUser(data);
+    io.to(data.roomId).emit("USERS_UPDATE", data.ownName);
+    console.log("disconnected ", socket.ownName);
+  });
 });
 
 // Test Route
-app.get('/', (req, res) => {
-  console.log('Route hit!');
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  console.log("Route hit!");
+  res.send("Hello, World!");
 });
 
 // API Routes
-app.use('/api', Api_Routes);
+app.use("/api", Api_Routes);
 
 server.listen(port, async () => {
   await connectDB();
-  console.log('Server Running On', port);
+  console.log("Server Running On", port);
   if (mongoose.connection.readyState !== 1) {
-    console.log('Not connected to MongoDB');
+    console.log("Not connected to MongoDB");
     return;
   }
 });
